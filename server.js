d764d831373324f022ab9a9fabb4f9c2278eaeff
Christopher de Beer <christopherdebeer@gmail.com>
2011-08-27 03:03:54 +0200

    change port to listen to

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.get('/now', function(req, res){
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
