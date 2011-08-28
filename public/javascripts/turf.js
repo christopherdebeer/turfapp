8d12c358621704487e332c3717957935e78c9220
Christopher de Beer <christopherdebeer@gmail.com>
2011-08-28 21:26:04 +0200

    getting my google maps v3 on, infowindows

--- public/javascripts/turf.js
+++ public/javascripts/turf.js
@@ -162,24 +162,13 @@ $(document).ready( function () {
 	var contentString = '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+    '<h1 id="firstHeading" class="firstHeading">Tag</h1>'+
     '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335 km (208 mi) '+
-    'south west of the nearest large town, Alice Springs; 450 km '+
-    '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
+    '<p><b>This is a generic tag marker window for now, im working on it</b>'+
     '</div>'+
     '</div>';
 
-	var turf.infowindow = new google.maps.InfoWindow({
+	turf.infowindow = new google.maps.InfoWindow({
 	    content: contentString
 	});
 
